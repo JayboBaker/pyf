bfd08b6e192c3472d0e84063790aa63f15a5044b
James Baker <look@jamesbaker.mer.uk>
2019-11-14 13:40:28 +0000

    JB - Remove unecessary icons

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -9,7 +9,7 @@ class Form extends React.Component {
   static defaultProps = {
     name: 'Simple Form Ajax',
     subject: 'Website Notification', // optional subject of the notification email
-    action: '',
+    action: '/',
     successMessage: 'Thanks for your enquiry, we will get back to you soon',
     errorMessage:
       'There is a problem, your message has not been sent, please try contacting us via email'
