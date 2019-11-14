4743bd2a197e2a01780681935b38d0504440468c
James Baker <look@jamesbaker.mer.uk>
2019-11-14 21:20:39 +0000

    JB - Add missing form name. Codetidy

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -7,8 +7,8 @@ import './Form.css'
 
 const encode = (data) => {
   return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
+      .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      .join('&');
 }
 
 class Form extends React.Component {
@@ -36,7 +36,10 @@ class Form extends React.Component {
     fetch(form.action, {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode(this.state.values)
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...this.state.values,
+      }),
     })
       .then(res => {
         if (res.ok) {
@@ -74,68 +77,68 @@ class Form extends React.Component {
     return (
       <Fragment>
         <Helmet>
-          {/* <script src="http://www.google.com/recaptcha/api.js" /> */}
+          {/* <script src='http://www.google.com/recaptcha/api.js' /> */}
         </Helmet>
         <form
-          className="Form"
+          className='Form'
           name={name}
           action={action}
           onSubmit={this.handleSubmit}
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          // netlify-recaptcha=""
+          data-netlify='true'
+          data-netlify-honeypot='bot-field'
+          // netlify-recaptcha='
         >
           {this.state.alert && (
-            <div className="Form--Alert">{this.state.alert}</div>
+            <div className='Form--Alert'>{this.state.alert}</div>
           )}
-          <div className="Form--Group">
-            <label className="Form--Label">
+          <div className='Form--Group'>
+            <label className='Form--Label'>
               <input
-                className="Form--Input Form--InputText"
-                type="text"
-                placeholder="Firstname"
-                name="firstname"
+                className='Form--Input Form--InputText'
+                type='text'
+                placeholder='Firstname'
+                name='firstname'
                 required
                 onChange={this.handleChange}
               />
               <span>Firstname</span>
             </label>
-            <label className="Form--Label">
+            <label className='Form--Label'>
               <input
-                className="Form--Input Form--InputText"
-                type="text"
-                placeholder="Lastname"
-                name="lastname"
+                className='Form--Input Form--InputText'
+                type='text'
+                placeholder='Lastname'
+                name='lastname'
                 onChange={this.handleChange}
               />
               <span>Lastname</span>
             </label>
           </div>
-          <label className="Form--Label">
+          <label className='Form--Label'>
             <input
-              className="Form--Input Form--InputText"
-              type="email"
-              placeholder="Email"
-              name="emailAddress"
+              className='Form--Input Form--InputText'
+              type='email'
+              placeholder='Email'
+              name='emailAddress'
               onChange={this.handleChange}
             />
             <span>Email address</span>
           </label>
-          <label className="Form--Label">
+          <label className='Form--Label'>
             <input
-              className="Form--Input Form--InputText"
-              type="text"
-              placeholder="Phone"
-              name="phoneNumber"
+              className='Form--Input Form--InputText'
+              type='text'
+              placeholder='Phone'
+              name='phoneNumber'
               onChange={this.handleChange}
             />
             <span>Phone Number</span>
           </label>
-          <label className="Form--Label has-arrow">
+          <label className='Form--Label has-arrow'>
             <select
-              className="Form--Input Form--Select"
-              name="type"
-              defaultValue="Type of Enquiry"
+              className='Form--Input Form--Select'
+              name='type'
+              defaultValue='Type of Enquiry'
               onChange={this.handleChange}
             >
               <option disabled hidden>
@@ -146,37 +149,37 @@ class Form extends React.Component {
               <option>Sub-contracting</option>
             </select>
           </label>
-          <input type="hidden" name="bot-field" />
-          <label className="Form--Label">
+          <input type='hidden' name='bot-field' />
+          <label className='Form--Label'>
             <textarea
-              className="Form--Input Form--Textarea Form--InputText"
-              placeholder="Message"
-              name="message"
-              rows="10"
+              className='Form--Input Form--Textarea Form--InputText'
+              placeholder='Message'
+              name='message'
+              rows='10'
               required
               onChange={this.handleChange}
             />
             <span>Message</span>
           </label>
-          <label className="Form--Label Form-Checkbox">
+          <label className='Form--Label Form-Checkbox'>
             <input
-              className="Form--Input Form--Textarea Form--CheckboxInput"
-              name="newsletter"
-              type="checkbox"
+              className='Form--Input Form--Textarea Form--CheckboxInput'
+              name='newsletter'
+              type='checkbox'
               onChange={this.handleChange}
             />
             <span>Get news updates</span>
           </label>
           {/* <div
-            className="g-recaptcha"
-            data-sitekey="6LfMVsIUAAAAAO7fIKvzJeQ33WidT75jOYUR_sl3"
+            className='g-recaptcha'
+            data-sitekey='6LfMVsIUAAAAAO7fIKvzJeQ33WidT75jOYUR_sl3'
           /> */}
-          {!!subject && <input type="hidden" name="subject" value={subject} />}
-          <input type="hidden" name="form-name" value={name} />
+          {!!subject && <input type='hidden' name='subject' value={subject} />}
+          <input type='hidden' name='form-name' value={name} />
           <input
-            className="Button Form--SubmitButton"
-            type="submit"
-            value="Enquire"
+            className='Button Form--SubmitButton'
+            type='submit'
+            value='Enquire'
             disabled={this.state.disabled}
           />
         </form>
